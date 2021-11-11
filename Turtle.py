15addb64dcfc08921e2fdbeedc1c08db912e5f1c
dilipkumar56 <94101426+dilipkumar56@users.noreply.github.com>
2021-11-11 12:26:32 +0530

    Add files via upload

---
+++ Turtle.py
@@ -0,0 +1,30 @@
+import turtle
+t=turtle.Turtle()
+t.speed(1)
+t.pensize(10)
+t.color("green")
+t.hideturtle()
+
+
+t.forward(1)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+#second letter
+
+t.penup()
+t.back(100)
+t.left(50)
+t.forward(50)
+t.pendown()
+
+
+#initise
+t.forward(100)0
+
+
